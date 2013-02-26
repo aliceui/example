c0b2514ca6498b336aaab8bc6d65c8f73fff0a20
偏右 <afcstar@163.com>
2013-02-26 11:07:43 +0800

    update

---
+++ dist/function-debug.js
@@ -0,0 +1,3 @@
+define("alice/example/1.0.0/function-debug", [], function(require, exports, module) {
+    seajs.importStyle('@import url("button");@import url("tipbox");@import url("./inner.css");.ui-example{}', "alice/example/1.0.0/example.css");
+});
---
+++ dist/function.js
@@ -0,0 +1,1 @@
+define("alice/example/1.0.0/function",[],function(){seajs.importStyle('@import url("button");@import url("tipbox");@import url("./inner.css");.ui-example{}',"alice/example/1.0.0/example.css")});
